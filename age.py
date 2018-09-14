7035e2214e1aa41ba130db31d52f6f25e3e8e4a7
Aarya09 <43162480+Aarya09@users.noreply.github.com>
2018-09-14 15:59:14 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,9 @@
+#program to cheak age for vote 
+#input a from keyboard 
+a=int(input("a:"))
+#print a from keyboard 
+print("a:",a)
+if(a>=18):
+	print(a,"is eligible for vote ",a)
+else:
+	print(a,"is not eligible for vote ")
