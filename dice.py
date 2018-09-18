36f8354dbaf03ca59fcabf3dbfa63d5b4921efb6
Aarya09 <43162480+Aarya09@users.noreply.github.com>
2018-09-18 10:33:00 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,3 @@
+#program to roll and cheak the dice value  
+import random
+print(random.randint(1,6))
---
+++ nthable.py
@@ -0,0 +1,9 @@
+#ask the user for a number and
+#print the table of that numbe till 
+a=int (input("enter the value of n:"))
+i=1
+while(i<=10):
+	b=a*i
+	print(a,"*",i,"=",b)
+	i=i+1
+	
