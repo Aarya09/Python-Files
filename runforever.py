963f13fa2f9b39454dd62680f89e6819c2f65447
Arun Kumar <43162480+Aarya09@users.noreply.github.com>
2025-11-25 15:21:14 +0530

    Delete runforever.py

--- runforever.py
+++
@@ -1,9 +0,0 @@
-#program to roll and quit the dice
-import random
-while True:
-	i=input("enter r to roll or q to quit:")
-	if (i=='r'):
-		print(random.randint(1,20))
-		exit()
-	else:
-		print("try again")
