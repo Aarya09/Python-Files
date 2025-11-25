921fd3834fff062c5f127245d46c1120cf581d67
Arun Kumar <43162480+Aarya09@users.noreply.github.com>
2025-11-25 15:18:12 +0530

    Delete age.py

--- age.py
+++
@@ -1,9 +0,0 @@
-#program to cheak age for vote 
-#input a from keyboard 
-a=int(input("a:"))
-#print a from keyboard 
-print("a:",a)
-if(a>=18):
-	print(a,"is eligible for vote ",a)
-else:
-	print(a,"is not eligible for vote ")
